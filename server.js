ebec8891ee190e444defafcc53e559e6a1e950e3
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-07-10 13:44:27 +0530

    Handle new user

--- server.js
+++ server.js
@@ -4,9 +4,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const shortid = require('shortid');
 
 const app = express();
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect('mongodb://localhost/test' || process.env.MONGO_URI);
+
+// Schemas
+const newUserSchema = mongoose.Schema({
+  name: String,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:')); // eslint-disable-line
+
 // Middleware
 app.use(express.static(`${__dirname}public`));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,6 +26,17 @@ app.get('/', (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.post('/api/new-user', (request, response) => {});
+app.post('/api/new-user', (request, response) => {
+  db.on('open', () => {
+    const NewUser = mongoose.model('NewUser', newUserSchema);
+    const id = shortid.generate();
+    const newUser = new NewUser({ name: request.body.name, id });
+    newUser.save((err) => {
+      if (err) return console.error(err); // eslint-disable-line
+      return true;
+    });
+    response.json(newUser);
+  });
+});
 
 app.listen(process.env.PORT || 3000);
