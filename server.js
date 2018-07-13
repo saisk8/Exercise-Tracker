032b79b4a517acd2a6d0ca613203d3d72914f333
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-07-13 13:36:50 +0530

    CSS files are now served

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:')); // eslint-disable-line
 
 // Middleware
-app.use(express.static('public'));
+app.use('/css', express.static('public/css'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
